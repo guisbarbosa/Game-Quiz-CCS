d805e843b03d7791a29a07dbe092185fac8ce1cb
eduardomdc <91088412+eduardomdc@users.noreply.github.com>
2022-10-28 18:39:57 -0300

    instanciamento automatico de opções de pergunta

--- Assets/Scripts/DisplayPergunta.cs
+++ Assets/Scripts/DisplayPergunta.cs
@@ -7,25 +7,42 @@ public class DisplayPergunta : MonoBehaviour
 {
     public Pergunta pergunta;
 
-    public TMP_Text titulo;
-    public TMP_Text[] respostas;
+    public TMP_Text prefabTitulo;
+    public TMP_Text prefabOpcao;
+    private List<TMP_Text> respostas;
+
+    private float tituloPosY = 250f;
+    private float tituloPosX = 0f;
+
+    private float respostasPosXInicial = -200f;
+    private float respostasPosY;
+    private float respostasPosYInicial = 100f;
+    private float respostasPosYIncremento = -50f;
     // Start is called before the first frame update
     void Start()
     {
+        respostas = new List<TMP_Text>();
         SetTexto();
     }
 
     private void SetTexto(){
-        titulo.text = pergunta.titulo;
+
+        SetTitulo();
+        respostasPosY = respostasPosYInicial;
         for (int i = 0; i < pergunta.respostas.Length; i++)
         {
-            if (i < respostas.Length){
-                respostas[i].text = Letra(i)+") " + pergunta.respostas[i];
-            }
-            else {
-                Debug.Log("Display de opção " + Letra(i) + " não encontrada");
-            }
+            TMP_Text opcao = Instantiate(prefabOpcao, transform);
+            opcao.text = Letra(i)+ ") " + pergunta.respostas[i];
+            opcao.transform.localPosition = new Vector3(respostasPosXInicial, respostasPosY, 0f);
+            respostas.Add(opcao);
+            respostasPosY += respostasPosYIncremento;
         }
+    }
+
+    private void SetTitulo(){
+        TMP_Text titulo = Instantiate(prefabTitulo, transform);
+        titulo.text = pergunta.titulo;
+        titulo.transform.localPosition = new Vector3(tituloPosX, tituloPosY, 0f);
     }
 
     private string Letra(int i){
